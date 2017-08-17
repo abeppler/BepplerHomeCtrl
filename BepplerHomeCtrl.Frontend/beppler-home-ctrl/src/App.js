ccbc231c5703ebe4d4714d8a9a5c1ebcb8f5a5ee
Anderson Beppler <abeppler2000@gmail.com>
2017-08-17 10:45:19 -0300

    retirada textos iniciais CRA; criação dos boxes

--- BepplerHomeCtrl.Frontend/beppler-home-ctrl/src/App.js
+++ BepplerHomeCtrl.Frontend/beppler-home-ctrl/src/App.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'admin-lte/bootstrap/css/bootstrap.min.css';
+import 'admin-lte/dist/css/AdminLTE.min.css'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div> 
+        <div className="App">
+          <div className="App-header">
+            <div className="row">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="wrapper">
+          <div className="content">
+            <div className="box box-default">
+              <div className="box-header with-border">
+                Meu Header
+              </div>
+              <div className="box-body">
+                <div className="row">
+                  <div className="col-md-6">
+                    Eita col1
+                  </div>
+                  <div className="col-md-6">
+                    Eita col2
+                  </div>                  
+                </div>
+              </div>
+              Hello AdminLTE World!
+            </div>            
+          </div>
+        </div>
       </div>
     );
   }
